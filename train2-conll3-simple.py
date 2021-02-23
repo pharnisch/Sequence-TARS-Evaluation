9a60708f2690f661f0119a623ae37d9d4069141f
Philipp Harnisch <p.harnisch.privat@gmail.com>
2021-02-23 12:30:10 +0100

    .

---
+++ train2-conll3-simple.py
@@ -0,0 +1,42 @@
+import sys
+import os
+sys.path.insert(0, "/vol/fob-vol7/mi19/harnisph/flair")
+
+import flair
+import torch
+from flair.data import Corpus
+from flair.models import SequenceTagger, TARSSequenceTagger, TARSSequenceTagger2
+from flair.trainers import ModelTrainer
+from flair.embeddings import WordEmbeddings, TransformerWordEmbeddings, TransformerDocumentEmbeddings
+from flair.data import Sentence
+from flair.data import MultiCorpus
+from flair.datasets import CONLL_03
+
+flair.set_seed(1)
+
+label_name_map = {
+"LOC":"Location","PER":"Person","ORG":"Organization","MISC":"Miscellaneous"
+}
+
+print(label_name_map)
+corpus = CONLL_03(tag_to_bioes=None, tag_to_bio2="ner", label_name_map=label_name_map, base_path="/vol/fob-vol7/mi19/harnisph/studienprojekt-dokumentation")
+corpus = corpus.downsample(0.1)
+
+tag_type = "ner"
+label_dictionary = corpus.make_label_dictionary(tag_type)
+print(label_dictionary)
+
+tagger = TARSSequenceTagger2(tag_dictionary=label_dictionary, tag_type=tag_type, task_name="TEST_NER")
+
+trainer = ModelTrainer(tagger, corpus, optimizer=torch.optim.AdamW)
+from torch.optim.lr_scheduler import OneCycleLR
+trainer.train(
+    base_path='resources/v1/conll_3-simple-2',
+    learning_rate=5.0e-5,
+    mini_batch_size=8,
+    mini_batch_chunk_size=None,
+    max_epochs=10,
+    weight_decay=0.,
+    embeddings_storage_mode="none",
+    scheduler=OneCycleLR,
+)
